b50452698082c682e2cc39e2656d2cef48ec4340
HuiiBuh <46561026+HuiiBuh@users.noreply.github.com>
2020-04-01 19:30:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
+import json
 with open('/home/runner/work/Action/Action/cookies.json') as file:
     a = file.read()
-    print(a.replace(" ", "")
+    print(a.replace(" ", ""))
     cookie = json.load(file)
     cookies = SpotifyCookies(cookie['sp_t'], cookie['sp_dc'], cookie['sp_key'])
