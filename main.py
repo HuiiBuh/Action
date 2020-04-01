297cd397960d1c728fc491c3626aed240ad27bb7
HuiiBuh <46561026+HuiiBuh@users.noreply.github.com>
2020-04-01 19:38:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,5 @@ import logging
 
 with open('/home/runner/work/Action/Action/cookies.json') as file:
     text = file.read()
-    print(text)
-    print(text.replace(" ", ""))
     cookie = json.loads(text)
-    cookie = json.load(file)
     print(cookie)
